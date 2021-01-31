14c23d87dc685d676421f2c2fc3f21ecf3a0492a
Paolo Acchiardi <48619429+paoloacchiardi@users.noreply.github.com>
2021-01-31 10:47:17 +0100

    Update client_web.py
    login website, mysql db, client bruteforce algorithm, POST request

--- web_es/client_web.py
+++ web_es/client_web.py
@@ -1,5 +1,19 @@
 import socket as sck
 
+def increase_pw(pw, last):
+    pw[last] += 1 #calculate next pw
+    if pw[last] == 255:
+        pw[last] = 32
+        t = 1
+        while (pw[last-t] == 255) and (t <= last):
+            pw[last-t] = 32
+            t += 1
+        if pw[0] == 32:
+            last += 1
+            pw[last] = 32
+        pw[last-t] += 1
+    return pw,last
+
 def main():
     #POST request, pdf online = https://github.com/conradis/TPSIT-classi-quinte/blob/main/Lezione_4-HTTP-aa1718.pdf
     client = sck.socket(sck.AF_INET,sck.SOCK_STREAM)
@@ -23,18 +37,7 @@ def main():
         client.sendall(mex.encode())
         data = client.recv(4096)
         print(data.decode())
-        pw[last] += 1 #calculate next pw
-        if pw[last] == 255:
-            pw[last] = 32
-            t = 1
-            while (pw[last-t] == 255) and (t <= last):
-                pw[last-t] = 32
-                t += 1
-            if pw[0] == 32:
-                last += 1
-                pw[last] = 32
-            else:
-                pw[last-t] += 1
+        pw, last = increase_pw(pw, last)
     client.close() 
 
 if __name__=="__main__":
